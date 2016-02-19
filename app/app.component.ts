07afa3fe9801a09d05e6d27343d710efd96c7fa9
Brittni <bgustaf@umich.edu>
2016-02-19 00:08:15 -0800

    Added parallax header component
    The parallax header component is a resuable header with a background
    image that scrolls slower than the element below the header.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,12 @@
 import {Component} from 'angular2/core';
+import {ParallaxHeaderComponent} from './components/parallax-header/parallax-header.component';
 
 @Component({
-    selector: 'b-and-moj',
-    template: '<h1>Oh Hai :D hi jordan</h1>'
+    selector:   'b-and-moj',
+    template:   '<parallax-header></parallax-header>' +
+                '<main></main>',
+    styleUrls:  ['app/app.css'],
+    directives: [ParallaxHeaderComponent]
 })
 
 export class AppComponent { }
---
+++ app/components/parallax-header/parallax-header.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'parallax-header',
+    template: '<header><h1>Oh Hai</h1></header>',
+    styleUrls: ['app/components/parallax-header/parallax-header.css']
+})
+
+export class ParallaxHeaderComponent { }
