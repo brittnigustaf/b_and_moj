28cc235a1b557506ee5d6e6e103ca457304f2950
Brittni Gustaf <bgustaf@umich.edu>
2017-05-20 11:40:50 -0700

    Replace Angular2 with ReactJS
    This frontend build environment was created using Facebook's
    [Create React App](https://github.com/facebookincubator/create-react-app)
    
    Main dependencies include Webpack, Babel, ESLint, etc.
    
    This commit also converts the parallax-header component to be a react
    component instead of an angular component.
    
    Note: the parallax-header component no longer works. A fix will be
    completed in a later commit.

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import ParallaxHeader from './components/parallax-header/parallax-header.js';
+
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <ParallaxHeader />
+        <p className="app-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/parallax-header/parallax-header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './parallax-header.css';
+
+class ParallaxHeader extends Component {
+    render() {
+        return (
+            <header className="parallax-header">
+                <h1>Oh Hai</h1>
+            </header>
+        )
+    }
+}
+
+export default ParallaxHeader;
