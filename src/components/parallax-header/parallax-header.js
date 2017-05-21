8db96f4273fcc7c0289e6ab29ec4b9032c6eab7a
Brittni Gustaf <bgustaf@umich.edu>
2017-05-20 19:49:43 -0700

    Add Javascript Prettier Pre-commit Hook
    Prettier is an opinionated JavaScript formatter that will automatically
    make linting changes to the JavaScript to enforce style. This project
    has been setup so that these linting changes are automatically made to
    staged files when `git commit` is performed.
    
    For more information, please see [https://prettier.github.io/prettier/]

--- src/components/parallax-header/parallax-header.js
+++ src/components/parallax-header/parallax-header.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import './parallax-header.css';
 
 class ParallaxHeader extends Component {
-    render() {
-        return (
-            <header className="parallax-header">
-                <h1>Oh Hai</h1>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header className="parallax-header">
+        <h1>Oh Hai</h1>
+      </header>
+    );
+  }
 }
 
 export default ParallaxHeader;
